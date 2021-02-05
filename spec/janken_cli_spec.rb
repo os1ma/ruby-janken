441e2b64f1fe9caf9855cc6f94bf9d43da65fca5
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-02-05 18:44:45 +0900

    パラメタライズドテストを導入

--- spec/janken_cli_spec.rb
+++ spec/janken_cli_spec.rb
@@ -3,27 +3,51 @@ require 'stringio'
 
 RSpec.describe 'janken_cli' do
   describe '#main' do
-    it 'able to play janken' do
-      $stdin = StringIO.new("0\n1")
-      $stdout = StringIO.new
-      main
-      actual = $stdout.string
+    where(
+      :player_1_hand_num,
+      :player_2_hand_num,
+      :player_1_hand_name,
+      :player_2_hand_name,
+      :result_message,
+      :player_1_result,
+      :player_2_result
+    ) do
+      [
+        [0, 0, 'STONE', 'STONE', 'DRAW !!!', 2, 2],
+        [0, 1, 'STONE', 'PAPER', 'Bob win !!!', 1, 0],
+        [0, 2, 'STONE', 'SCISSORS', 'Alice win !!!', 0, 1],
+        [1, 0, 'PAPER', 'STONE', 'Alice win !!!', 0, 1],
+        [1, 1, 'PAPER', 'PAPER', 'DRAW !!!', 2, 2],
+        [1, 2, 'PAPER', 'SCISSORS', 'Bob win !!!', 1, 0],
+        [2, 0, 'SCISSORS', 'STONE', 'Bob win !!!', 1, 0],
+        [2, 1, 'SCISSORS', 'PAPER', 'Alice win !!!', 0, 1],
+        [2, 2, 'SCISSORS', 'SCISSORS', 'DRAW !!!', 2, 2]
+      ]
+    end
 
-      expected = <<~TEXT
-        STONE: 0
-        PAPER: 1
-        SCISSORS: 2
-        Please select Alice hand:
-        STONE: 0
-        PAPER: 1
-        SCISSORS: 2
-        Please select Bob hand:
-        Alice selected STONE
-        Bob selected PAPER
-        Bob win !!!
-      TEXT
+    with_them do
+      it "正常な入力でじゃんけんが実行され結果が保存される" do
+        $stdin = StringIO.new("#{player_1_hand_num}\n#{player_2_hand_num}")
+        $stdout = StringIO.new
+        main
+        actual = $stdout.string
 
-      expect(actual).to eq(expected)
+        expected = <<~TEXT
+          STONE: 0
+          PAPER: 1
+          SCISSORS: 2
+          Please select Alice hand:
+          STONE: 0
+          PAPER: 1
+          SCISSORS: 2
+          Please select Bob hand:
+          Alice selected #{player_1_hand_name}
+          Bob selected #{player_2_hand_name}
+          #{result_message}
+        TEXT
+
+        expect(actual).to eq expected
+      end
     end
   end
 end
