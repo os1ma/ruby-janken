5d1a16e6d5da5adbe2be16598cc6672ca215b8cb
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-02-06 23:21:21 +0900

    janken_detail_csv_dao.rb を RuboCop をパスするよう修正

--- app/daos/csv/janken_detail_csv_dao.rb
+++ app/daos/csv/janken_detail_csv_dao.rb
@@ -7,17 +7,23 @@ require './app/daos/csv/csv_dao_utils'
 class JankenDetailCsvDao
   JANKEN_DETAILS_CSV = "#{CsvDaoUtils::DATA_DIR}/janken_details.csv"
 
-  def insert_all(janken_details) # rubocop:disable Metrics/AbcSize
+  def insert_all(janken_details)
     FileUtils.touch(JANKEN_DETAILS_CSV)
 
-    max_janken_detail_id_before_insert = CsvDaoUtils.count_file_lines(JANKEN_DETAILS_CSV)
+    starting_id = CsvDaoUtils.count_file_lines(JANKEN_DETAILS_CSV) + 1
 
     CSV.open(JANKEN_DETAILS_CSV, 'a') do |csv|
-      janken_details.map.with_index do |jd, index|
-        janken_detail_id = max_janken_detail_id_before_insert + index + 1
-        csv << [janken_detail_id, jd.janken_id, jd.player_id, jd.hand.number, jd.result.number]
-        JankenDetail.new(janken_detail_id, jd.janken_id, jd.player_id, jd.hand, jd.result)
-      end
+      insert_all_with_starting_id(janken_details, starting_id, csv)
+    end
+  end
+
+  private
+
+  def insert_all_with_starting_id(janken_details, starting_id, csv)
+    janken_details.map.with_index do |jd, index|
+      janken_detail_id = starting_id + index
+      csv << [janken_detail_id, jd.janken_id, jd.player_id, jd.hand.number, jd.result.number]
+      JankenDetail.new(janken_detail_id, jd.janken_id, jd.player_id, jd.hand, jd.result)
     end
   end
 end
