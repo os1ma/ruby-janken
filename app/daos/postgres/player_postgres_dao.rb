1c83e57913bed0e77a5b3c0bfa7c45079572ba1c
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-02-08 22:47:12 +0900

    PostgreSQL の PREPARE を使うよう修正

--- app/daos/postgres/player_postgres_dao.rb
+++ app/daos/postgres/player_postgres_dao.rb
@@ -4,13 +4,12 @@ require './app/daos/postgres/postgres_dao_utils'
 
 # プレイヤーを PosgtreSQL と読み書きする DAO
 class PlayerPostgresDao
-  SELECT_WHERE_ID_EQUALS_QUERY = 'SELECT "id", "name" FROM "players" WHERE "id" = %s'
+  SELECT_WHERE_ID_EQUALS_QUERY = 'SELECT "id", "name" FROM "players" WHERE "id" = $1'
 
   def find_player_by_id(player_id)
-    query = format(SELECT_WHERE_ID_EQUALS_QUERY, player_id)
-
     PostgresDaoUtils.with_connection do |conn|
-      res = conn.exec(query)
+      conn.prepare(SELECT_WHERE_ID_EQUALS_QUERY, SELECT_WHERE_ID_EQUALS_QUERY)
+      res = conn.exec_prepared(SELECT_WHERE_ID_EQUALS_QUERY, [player_id])
       res.map { |r| Player.new(r['id'].to_i, r['name']) }.first
     end
   end
